99a6553c61f70561ae74407dc5ef9ad1320538a6
Hafs Ibrahim <hafs.darwish@gmail.com>
2020-12-07 20:31:15 +0200

    Completed module

---
+++ api/app.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let path = require('path');
+
+const app = express();
+const pathToIndex=path.resolve(__dirname, '../client/index.html');
+app.use('/*', (request, response)=>{
+  response.sendFile(pathToIndex);
+});
+module.exports=app;
